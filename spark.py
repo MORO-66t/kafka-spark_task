13763f83b2d70e5776d0f8427fc1587e5a01a347
MORO-66t <166075118+MORO-66t@users.noreply.github.com>
2024-12-08 00:10:13 +0200

    push full project

--- spark.py
+++ spark.py
@@ -1,51 +1,50 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, avg
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType , IntegerType
-# from pyspark.sql.streaming import Trigger
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import avg
 
-# Initialize Spark Session
 spark = SparkSession.builder \
-    .appName("ProductReviewSummary") \
+    .appName("sparkConsumer") \
+    .master("local[*]") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,"
+            "mysql:mysql-connector-java:8.0.40") \
     .getOrCreate()
 
-# Define schema for data
+
 schema = StructType([
     StructField("primaryCategories", StringType(), True),
-    StructField("reviews", StructType([
-        StructField("rating", DoubleType(), True)
-    ]))
+    StructField("reviews_rating",DoubleType() , True)
 ])
 
-# Read from Kafka
-df = spark.readStream \
-    .format("kafka") \
+df = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
     .option("subscribe", "product-reviews") \
+    .option("startingOffsets", "latest") \
     .load()
+parsed_df = df.selectExpr("CAST(value AS STRING) as json") \
+    .select(from_json(col("json"), schema).alias("data")) \
+    .select("data.primaryCategories", "data.reviews_rating")
 
-# Parse JSON data
-parsed_df = df.selectExpr("CAST(value AS STRING)") \
-    .select(from_json(col("value"), schema).alias("data")) \
-    .select("data.*")
 
-# Calculate average review rating by category
-result_df = parsed_df.groupBy("primaryCategories").agg(avg("reviews.rating").alias("avg_rating"))
+flattened_df = parsed_df.select(
+    col("primaryCategories").alias("primaryCategories"),
+    col("reviews_rating").alias("reviews_rating")
+)
 
-# Write to MySQL
+transformed_df = flattened_df.groupBy("primaryCategories").agg(avg("reviews_rating").cast("double").alias("reviews_rating"))
+
 def write_to_mysql(batch_df, batch_id):
-    batch_df.write \
-        .format("jdbc") \
-        .option("url", "jdbc:mysql://localhost:3306/reviewsdb") \
-        .option("driver", "com.mysql.cj.jdbc.Driver") \
-        .option("dbtable", "average_ratings") \
-        .option("user", "root") \
-        .option("password", "") \
-        .mode("append") \
-        .save()
+    jdbc_url = "jdbc:mysql://localhost:3306/reviewsdb"
+    properties = {
+        "user": "root",
+        "password": "0plm0plM@",
+        "driver": "com.mysql.cj.jdbc.Driver"
+    }
+    batch_df.show()
+    batch_df.write.jdbc(url=jdbc_url, table="average_ratings", mode="append", properties=properties)
 
-query = result_df.writeStream \
-    .outputMode("update") \
+    
+query = transformed_df.writeStream \
+    .outputMode("complete") \
     .foreachBatch(write_to_mysql) \
-    .start()
-
-# query.awaitTermination()
+    .start().awaitTermination() 
